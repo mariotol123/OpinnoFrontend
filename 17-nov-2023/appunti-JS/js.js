4e4104a3b0eb0f8ca3d9e2cf7e121068f37a1c99
Mario Tolvo <mariobros9@hotmail.it>
2023-11-20 14:18:08 +0100

    Inizio js

---
+++ 17-nov-2023/appunti-JS/js.js
@@ -0,0 +1,85 @@
+/* for (inizializzazione; condizione; incremento){
+        blocco di codice da eseguire
+    }
+}*/
+
+//for(let i = 3; i<=5; i++ ){
+   // console.log(i)
+//}
+
+/*
+ while (condizione){
+  blocco di codice da eseguire
+  }
+ */
+
+  //let i = 1;
+
+ // while(i > 0){
+    //console.log("ciao")
+  //}
+
+
+  /* 
+    do {
+        blocco di codice da eseguire
+    }  while (condizione);
+
+    Stavolta il blocco di codice e' prima del while, fai questa roba finche quest'altra
+  */
+
+
+    //var nome = prompt("Inserisci il tuo nome");
+
+    //document.write("<h1>Ciao, " + nome + "!<h1>")
+
+
+    let num1 = 3;
+    let num2 = 5;
+
+
+    let somma = num1 + num2;
+    let sott = num1 - num2;
+    let div = num1 / num2;
+    let molt = num1 * num2;
+    let resto = num1 % num2;
+
+    console.log(somma,sott,div,molt,resto);
+
+
+
+    let i;
+    
+
+    for(let i = 1; i < 11; i++){
+        console.log(i);
+    }
+
+
+    let n = 1;
+
+    if(n > 0){
+        console.log("numero positivo");
+    } else if (n < 0){
+        console.log("Il numero e' negativo");
+    } else{
+        console.log("Non e' mai nato");
+    }
+
+
+    let num = 5;
+    let p;
+    do{
+         p = prompt("inserisci numero");
+         if(p == num){
+            console.log("Il numero e' giusto");
+        } else { console.log("sbagliato");
+
+            } 8
+
+    } while(p != num);
+
+
+
+
+
