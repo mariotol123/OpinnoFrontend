25f13a48aa2085edd2d04d4bf288f7a764806cb6
Mario Tolvo <mariobros9@hotmail.it>
2023-11-21 11:36:12 +0100

    js

--- 17-nov-2023/appunti-JS/js.js
+++ 17-nov-2023/appunti-JS/js.js
@@ -34,51 +34,53 @@
     //document.write("<h1>Ciao, " + nome + "!<h1>")
 
 
-    let num1 = 3;
-    let num2 = 5;
+    // let num1 = 3;
+    // let num2 = 5;
 
 
-    let somma = num1 + num2;
-    let sott = num1 - num2;
-    let div = num1 / num2;
-    let molt = num1 * num2;
-    let resto = num1 % num2;
+    // let somma = num1 + num2;
+    // let sott = num1 - num2;
+    // let div = num1 / num2;
+    // let molt = num1 * num2;
+    // let resto = num1 % num2;
 
-    console.log(somma,sott,div,molt,resto);
+    // console.log(somma,sott,div,molt,resto);
 
 
 
-    let i;
+    // let i;
     
 
-    for(let i = 1; i < 11; i++){
-        console.log(i);
-    }
+    // for(let i = 1; i < 11; i++){
+    //     console.log(i);
+    // }
 
 
-    let n = 1;
+    // let n = 1;
 
-    if(n > 0){
-        console.log("numero positivo");
-    } else if (n < 0){
-        console.log("Il numero e' negativo");
-    } else{
-        console.log("Non e' mai nato");
-    }
+    // if(n > 0){
+    //     console.log("numero positivo");
+    // } else if (n < 0){
+    //     console.log("Il numero e' negativo");
+    // } else{
+    //     console.log("Non e' mai nato");
+    // }
 
 
-    let num = 5;
-    let p;
-    do{
-         p = prompt("inserisci numero");
-         if(p == num){
-            console.log("Il numero e' giusto");
-        } else { console.log("sbagliato");
+    // let num = 5;
+    // let p;
+    // do{
+    //      p = prompt("inserisci numero");
+    //      if(p == num){
+    //         console.log("Il numero e' giusto");
+    //     } else { console.log("sbagliato");
 
-            } 8
+    //         } 8
 
-    } while(p != num);
+    // } while(p != num);
 
+
+    
 
 
 
